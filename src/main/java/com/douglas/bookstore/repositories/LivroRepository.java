d66f6f1e0794d998207fa25c5c0ce7cb12d74691
DevdouglasConectado <douglasrodriguesconectadoifmg@gmail.com>
2021-09-02 13:38:49 -0300

    repositories implementados

---
+++ src/main/java/com/douglas/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.douglas.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.douglas.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
